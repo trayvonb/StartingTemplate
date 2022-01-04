a4a3de3a10ad4167ff970f0bd35d7e320c7a685d
Tray <93352324+trayvonb@users.noreply.github.com>
2022-01-04 16:57:55 -0600

    Update form.py

--- form.py
+++ form.py
@@ -2,5 +2,5 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 class MessageForm(FlaskForm):
-    message = StringField('Leave a message')
+    message = StringField('how was your day')
     submit = SubmitField('Send')
