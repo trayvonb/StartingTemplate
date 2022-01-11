f01beffa0764528e0e9566bcfaa7fc64dad54bf6
Tray <93352324+trayvonb@users.noreply.github.com>
2022-01-11 23:17:57 +0000

    trayvon

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect
 from form import MessageForm
 from config import Config
 from Todo import my_function 
-
 app = Flask(
   'app',
   template_folder='templates'
@@ -10,14 +9,20 @@ app = Flask(
 app.config.from_object(Config)
 
 my_message = ""
-def setMessage(message):
+def setMessage(my_To_Do_List):
   global my_message
-  my_message=message
+  my_message="Trayvon"
   print(my_message)
+
+def my_function():
+  print("my_To_Do_")
+  my_To_Do_List=["clean my room, do my homework, wash my clothing, go to the mall"]
 
 @app.route('/', methods=['GET', 'POST'])
 def page_one():
+  print('my_To_Do_List')
   form = MessageForm()
+  print("my_To_Do_List")
   if form.is_submitted():
     print("made it")
     setMessage(form.message.data)
@@ -29,5 +34,3 @@ def page_two():
   return render_template('pageTwo.html', my_message=my_message)
 
 app.run(host='0.0.0.0', port=8080)
-
-@app.route('/', methods=['get'])
